fb5f7f1a542ed5d98121ee18cf44d157a10e11f4
earlinn <earlinndrow@gmail.com>
2024-04-25 10:35:10 +0500

    Test user login using mocked token

--- 5-tests/tests/user/test_views.py
+++ 5-tests/tests/user/test_views.py
@@ -1,4 +1,5 @@
-from blog_app.user.models import User
+import uuid
+from blog_app.user.models import Session, User
 
 
 class TestRegisterView:
@@ -18,3 +19,16 @@ class TestRegisterView:
         response = await cli.post("/user.register", json=data)
         assert response.status == 400
         assert await response.json() == {"code": "user_already_exists"}
+
+
+class TestLoginView:
+    async def test_success(self, cli, user: User, mocker):
+        token = "0" * 32
+        mock_uuid = mocker.patch.object(uuid, "uuid4", autospec=True)
+        mock_uuid.return_value = uuid.UUID(hex=token)
+        data = {"username": user.username, "password": "1234"}
+        response = await cli.post("/user.login", json=data)
+        assert response.status == 200
+        session = await Session.query.where(Session.key == token).gino.first()
+        assert session.user_id == user.id
+        assert await response.json() == {"token": token}
